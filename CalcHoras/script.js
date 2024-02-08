9e50f7cb3f7d68d3db62db373ffae74cf47475a8
Luiza Fischer <127206345+LuizaFischer@users.noreply.github.com>
2024-02-08 12:39:37 -0300

    Add files via upload

---
+++ CalcHoras/script.js
@@ -0,0 +1,20 @@
+let hi = document.getElementById("horaInicial");
+let mi = document.getElementById("minInicial");
+let hf = document.getElementById("horaFinal");
+let mf = document.getElementById("minFinal");
+let resultado = document.getElementById("resultado")
+
+function soma(){
+    let somaHora = Number(hi.value) + Number (hf.value);
+    let somaMinuto = Number(mi.value) + Number (mf.value);
+    while (somaMinuto > 59){
+        somaMinuto -= 60;
+        somaHora ++;
+    }
+
+    resultado.innerHTML = somaHora + ":" + somaMinuto
+}
+
+function sub(){
+
+}
